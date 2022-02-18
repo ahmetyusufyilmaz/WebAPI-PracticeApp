4886ce722f40ce0a572fd5bffd3d4c0cbb42e0ed
ahmetyusufyilmaz <94035519+ahmetyusufyilmaz@users.noreply.github.com>
2022-02-18 21:12:39 +0300

    added validator

--- WebAPI-Practices/Validations/ProductValidator.cs
+++ WebAPI-Practices/Validations/ProductValidator.cs
@@ -1,11 +1,22 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI_Practices.Entities.ViewModels;
 
 namespace WebAPI_Practices.Validations
 {
-    public class ProductValidator
+    public class ProductValidator : AbstractValidator<PostProductQueryViewModel>
     {
+        public ProductValidator()
+        {
+            RuleFor(p => p.ProductName).NotEmpty().MinimumLength(3).MaximumLength(20);
+
+            RuleFor(p => p.CategoryId).LessThanOrEqualTo(100).NotEmpty();
+
+            RuleFor(p => p.StockAmount).NotEmpty().GreaterThan(0);
+
+        }
     }
 }
